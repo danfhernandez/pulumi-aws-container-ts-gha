f16c91d1353b0789ea89034c5d0963ab466ccea2
Dan Hernandez <danfhern@gmail.com>
2020-06-11 14:40:15 -0400

    Adding pulumi program

--- index.ts
+++ index.ts
@@ -7,3 +7,10 @@ const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+Add this shell script to make it easier 
+git init
+git add .
+git commit -m "Adding pulumi program"
+git remote add origin git@github.com:danfhernandez/my-unique-template.git
+git push -u origin master
+                
